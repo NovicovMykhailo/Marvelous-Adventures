f889cf90daaa79541b41e3d257bfece6ee296e26
NovicovMykhailo <mikenovicov@gmail.com>
2023-11-14 13:52:58 +0200

    trying to test some gh-pages route bugs fixes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import ToastWrapper from 'elements/ToastWrapper';
 
 import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <BrowserRouter basename="/marvelous-adventures">
-    <App />
-    <ToastWrapper />
-  </BrowserRouter>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <HashRouter>
+      {/* <BrowserRouter basename="/marvelous-adventures"> */}
+        <App />
+        <ToastWrapper />
+      {/* </BrowserRouter> */}
+    </HashRouter>
+  </React.StrictMode>
 );

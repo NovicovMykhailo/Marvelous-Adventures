016d843dcc1bb52ffc456284041cbfc2b0246a80
NovicovMykhailo <mikenovicov@gmail.com>
2023-11-14 13:54:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import ToastWrapper from 'elements/ToastWrapper';
 
 import 'react-toastify/dist/ReactToastify.css';

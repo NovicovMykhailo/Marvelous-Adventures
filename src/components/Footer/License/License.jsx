a19db72ff4529c00667043fef2d6bdcb410b7c92
NovicovMykhailo <mikenovicov@gmail.com>
2023-12-01 18:40:47 +0200

    fix links

--- src/components/Footer/License/License.jsx
+++ src/components/Footer/License/License.jsx
@@ -3,7 +3,9 @@ import css from '../Footer.module.css';
 const License = () => {
   return (
     <div className={css.liscenceBlock}>
+      <a href="https://github.com/NovicovMykhailo" target="_blank" rel="noopener noreferrer" title="Developer's page">
       <h4 className={css.liscence}>Privacy Policy Public Offering Agreement</h4>
+      </a>
     </div>
   );
 };
--- src/elements/Socials/Socials.jsx
+++ src/elements/Socials/Socials.jsx
@@ -3,21 +3,22 @@ import insta from '../../images/insta.svg';
 import tweet from '../../images/tweet.svg';
 import css from './Socials.module.css';
 
+
 const Socials = () => {
   return (
     <ul className={css.socials}>
       <li >
-        <a className={css.elements} href="facebook.com" target="blank">
+        <a className={css.elements} href="https://www.facebook.com" target="blank" title="Move to Facebook">
           <img className={css.icons} src={fb} alt="fb" width="40" />
         </a>
       </li>
       <li >
-        <a className={css.elements} href="instagram.com" target="blank">
+        <a className={css.elements} href="https://www.instagram.com/" target="blank" title="Move to Instagram">
           <img className={css.icons} src={insta} alt="insta" width="40" />
         </a>
       </li>
       <li >
-        <a className={css.elements} href="tweeter.com" target="blank">
+        <a className={css.elements} href="https://twitter.com/" target="blank" title="Move to Twitter">
           <img className={css.icons} src={tweet} alt="tweet" width="40" />
         </a>
       </li>
